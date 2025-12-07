5115b6dcbc2ea3f21a8d3545fc2e2fc6c06f864c
macedera <acederamichael07@gmail.com>
2025-12-08 05:13:44 +0800

    feat(): construct payload based on requirements

--- src/app/api/grade/route.ts
+++ src/app/api/grade/route.ts
@@ -3,13 +3,14 @@ import { NextResponse, NextRequest } from "next/server";
 export async function POST(req: NextRequest) {
   const backendURL = process.env.BACKEND_URL;
   const body = await req.json();
+  const formData = { answers: body };
   try {
     const response = await fetch(`${backendURL}/grade`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(body),
+      body: JSON.stringify(formData),
     });
     const data = await response.json();
     return NextResponse.json({ response: data }, { status: 200 });
