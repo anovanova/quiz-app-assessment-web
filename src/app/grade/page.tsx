e33650ffd48f339e8f5ace58db03ad1586b5f7e9
macedera <acederamichael07@gmail.com>
2025-12-08 06:41:44 +0800

    feat(): add suspense react

--- src/app/grade/page.tsx
+++ src/app/grade/page.tsx
@@ -1,15 +1,15 @@
 "use client";
 
-import { Suspense } from "react";
 import { useSearchParams } from "next/navigation";
-export default function GradePage() {
+import { Suspense } from "react";
+function GradePage() {
   const searchParams = useSearchParams();
 
   const data = searchParams.get("data");
   const parsedData = JSON.parse(data!);
 
   return (
-    <Suspense fallback={<div>Loading search params...</div>}>
+    <Suspense fallback={<div>Loading search results...</div>}>
       <div className="flex min-h-screen items-center justify-center font-sans dark:bg-black">
         <main className="flex min-h-screen w-full max-w-3xl flex-col items-center py-32 px-16 bg-white dark:bg-black sm:items-start">
           <div className="w-full text-center pb-4">
@@ -44,3 +44,5 @@ export default function GradePage() {
     </Suspense>
   );
 }
+
+export default GradePage;
